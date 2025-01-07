1d2b3c7bd8495142e6e436f9ba294d3522f8dfc7
Slime <4sliman4@gmail.com>
2025-01-07 23:44:05 +0200

    Set up log_nep297 table

---
+++ migrations/20250107000002_set_up_log_nep297.sql
@@ -0,0 +1,25 @@
+CREATE TABLE log_nep297_events (
+    id BIGSERIAL,
+    block_height BIGINT NOT NULL,
+    block_timestamp_nanosec TIMESTAMPTZ NOT NULL,
+    transaction_id TEXT NOT NULL,
+    receipt_id TEXT NOT NULL,
+    account_id TEXT NOT NULL,
+    predecessor_id TEXT NOT NULL,
+    event_standard TEXT NOT NULL,
+    event_version TEXT NOT NULL,
+    event_event TEXT NOT NULL,
+    event_data JSONB
+);
+
+SELECT create_hypertable('log_nep297_events', 'block_timestamp_nanosec', 
+    chunk_time_interval => INTERVAL '1 day');
+
+CREATE INDEX idx_log_nep297_events_standard_event 
+    ON log_nep297_events (event_standard, event_event, block_timestamp_nanosec DESC);
+
+CREATE INDEX idx_log_nep297_events_standard_event_account 
+    ON log_nep297_events (event_standard, event_event, account_id, block_timestamp_nanosec DESC);
+
+CREATE INDEX idx_log_nep297_events_standard_event_account_version 
+    ON log_nep297_events (event_standard, event_event, account_id, event_version, block_timestamp_nanosec DESC);
