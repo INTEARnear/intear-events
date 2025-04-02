37ef364bd2c13ea7b6fe006b45c5401ce86537c7
Slime <4sliman4@gmail.com>
2025-04-03 00:49:46 +0300

    Add Veax pool type

--- src/events/trade/trade_pool_change.rs
+++ src/events/trade/trade_pool_change.rs
@@ -27,6 +27,7 @@ pub enum PoolType {
     Ref(RefPool),
     Aidols(AidolsPool),
     GraFun(GraFunPool),
+    Veax(VeaxPool),
 }
 
 #[allow(clippy::enum_variant_names)]
@@ -143,4 +144,22 @@ pub struct GraFunPool {
     pub wnear_hold: Balance,
     pub is_deployed: bool,
     pub is_tradable: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
+#[allow(dead_code)]
+pub struct VeaxPool {
+    #[schemars(with = "(String, String)")]
+    pub pool: (AccountId, AccountId),
+    pub r#type: String,
+    /// 8 values, for each fee tier, lowest to highest
+    #[serde(with = "dec_format_vec")]
+    #[schemars(with = "Vec<String>")]
+    pub amounts_a: Vec<Balance>,
+    /// 8 values, for each fee tier, lowest to highest
+    #[serde(with = "dec_format_vec")]
+    #[schemars(with = "Vec<String>")]
+    pub amounts_b: Vec<Balance>,
+    pub sqrt_prices: Vec<f64>,
+    pub liquidities: Vec<f64>,
 }
