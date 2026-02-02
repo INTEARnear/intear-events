6b9619f255a06f15f7af4d7516f05865b8dbf8f2
Slime <4sliman4@gmail.com>
2026-02-02 16:11:45 +0200

    Add intear plach

--- src/events/trade/trade_pool_change.rs
+++ src/events/trade/trade_pool_change.rs
@@ -1,3 +1,6 @@
+use std::fmt::{self, Display};
+use std::str::FromStr;
+
 use inindexer::near_indexer_primitives::types::{AccountId, BlockHeight};
 use inindexer::near_indexer_primitives::CryptoHash;
 use inindexer::near_utils::{dec_format, dec_format_vec, FtBalance};
@@ -26,6 +29,7 @@ impl TradePoolChangeEvent {
 pub enum PoolType {
     Ref(RefPool),
     Aidols(AidolsPool),
+    IntearPlach(IntearPlachPool),
 }
 
 #[allow(clippy::enum_variant_names)]
@@ -156,4 +160,129 @@ pub struct AidolsPool {
     pub wnear_hold: FtBalance,
     pub is_deployed: bool,
     pub is_tradable: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
+pub enum IntearPlachPool {
+    Private {
+        assets: (IntearAssetWithBalance, IntearAssetWithBalance),
+        fees: IntearPlachFeeConfiguration,
+        #[schemars(with = "String")]
+        owner_id: AccountId,
+    },
+    Public {
+        assets: (IntearAssetWithBalance, IntearAssetWithBalance),
+        fees: IntearPlachFeeConfiguration,
+        #[serde(with = "dec_format")]
+        #[schemars(with = "Option<String>")]
+        total_shares: Option<FtBalance>,
+    },
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
+pub struct IntearPlachFeeConfiguration {
+    receivers: Vec<(IntearPlachFeeReceiver, IntearPlachFeeFraction)>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
+pub enum IntearPlachFeeReceiver {
+    Account(#[schemars(with = "String")] AccountId),
+}
+
+type IntearPlachFeeFraction = u32;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
+pub struct IntearAssetWithBalance {
+    asset_id: IntearAssetId,
+    #[serde(with = "dec_format")]
+    #[schemars(with = "String")]
+    balance: FtBalance,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum IntearAssetId {
+    Near,
+    Nep141(AccountId),
+    Nep245(AccountId, String),
+    Nep171(AccountId, String),
+}
+
+impl Display for IntearAssetId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Near => write!(f, "near"),
+            Self::Nep141(contract_id) => write!(f, "nep141:{contract_id}"),
+            Self::Nep245(contract_id, token_id) => write!(f, "nep245:{contract_id}:{token_id}"),
+            Self::Nep171(contract_id, token_id) => write!(f, "nep171:{contract_id}:{token_id}"),
+        }
+    }
+}
+
+impl FromStr for IntearAssetId {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "near" => Ok(Self::Near),
+            _ => match s.split_once(':') {
+                Some(("nep141", contract_id)) => {
+                    Ok(Self::Nep141(contract_id.parse().map_err(|e| {
+                        format!("Invalid account id {contract_id}: {e}")
+                    })?))
+                }
+                Some(("nep245", rest)) => {
+                    if let Some((contract_id, token_id)) = rest.split_once(':') {
+                        Ok(Self::Nep245(
+                            contract_id
+                                .parse()
+                                .map_err(|e| format!("Invalid account id {contract_id}: {e}"))?,
+                            token_id.to_string(),
+                        ))
+                    } else {
+                        Err(format!("Invalid asset id: {s}"))
+                    }
+                }
+                Some(("nep171", rest)) => {
+                    if let Some((contract_id, token_id)) = rest.split_once(':') {
+                        Ok(Self::Nep171(
+                            contract_id
+                                .parse()
+                                .map_err(|e| format!("Invalid account id {contract_id}: {e}"))?,
+                            token_id.to_string(),
+                        ))
+                    } else {
+                        Err(format!("Invalid asset id: {s}"))
+                    }
+                }
+                _ => Err(format!("Invalid asset id: {s}")),
+            },
+        }
+    }
+}
+
+impl Serialize for IntearAssetId {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.to_string().serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for IntearAssetId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Self::from_str(&s).map_err(serde::de::Error::custom)
+    }
+}
+
+impl schemars::JsonSchema for IntearAssetId {
+    fn schema_name() -> String {
+        "AssetId".to_string()
+    }
+    fn json_schema(generator: &mut schemars::r#gen::SchemaGenerator) -> schemars::schema::Schema {
+        <String as schemars::JsonSchema>::json_schema(generator)
+    }
 }
